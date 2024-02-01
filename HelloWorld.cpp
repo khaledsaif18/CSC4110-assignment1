9ce9833c3311d71ab259d17d12855877b52d36dc
Khaled Saif <khaledsaif@wayne.edu>
2024-02-01 06:37:19 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Khaled Saif <khaledsaif@wayne.edu>
+// Header file for input output functions
 #include<iostream>  
   
 using namespace std; 
